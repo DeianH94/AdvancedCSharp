b4afc587c67f30f80b07155af2a10126788fb54d
DeianH94 <deianh94@gmail.com>
2016-03-13 21:44:14 +0200

    Added problems 7 and 8.

---
+++ 02.MatrixSetsAndDictioneries/MatixSetsAndDictionariesHomework/Problem07.Phonebook/PhonebookMain.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem07.Phonebook
+{
+    using System;
+    using System.Collections.Generic;
+
+    class PhonebookMain
+    {
+        static void Main()
+        {
+            string[] inputContact = Console.ReadLine().Split('-');
+            var phonebook = new Dictionary<string, List<string>>();
+
+            while (inputContact[0] != "search")
+            {
+                if (!phonebook.ContainsKey(inputContact[0]))
+                {
+                    phonebook[inputContact[0]] = new List<string>();
+                }
+
+                phonebook[inputContact[0]].Add(inputContact[1]);
+                inputContact = Console.ReadLine().Split('-');
+            }
+
+            string searchName = Console.ReadLine();
+            while (searchName != string.Empty)
+            {
+                if (phonebook.ContainsKey(searchName))
+                {
+                    Console.WriteLine("{0} -> {1}", searchName, string.Join(", ", phonebook[searchName]));
+                }
+                else
+                {
+                    Console.WriteLine("Contact {0} does not exist.", searchName);
+                }
+
+                searchName = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 02.MatrixSetsAndDictioneries/MatixSetsAndDictionariesHomework/Problem08.NightLife/NightLifeMain.cs
@@ -0,0 +1,50 @@
+﻿namespace Problem08.NightLife
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class NightLifeMain
+    {
+        static void Main()
+        {
+            string[] inputData = Console.ReadLine()
+                .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            var clubInfo = new Dictionary<string, SortedDictionary<string, SortedSet<string>>>();
+
+            while (inputData[0].ToLower() != "end")
+            {
+                string city = inputData[0];
+                string venue = inputData[1];
+                string performer = inputData[2];
+
+                if (!clubInfo.ContainsKey(city))
+                {
+                    clubInfo.Add(city, new SortedDictionary<string, SortedSet<string>>());
+                }
+
+                if (!clubInfo[city].ContainsKey(venue))
+                {
+                    clubInfo[city].Add(venue, new SortedSet<string>());
+                }
+
+                clubInfo[city][venue].Add(performer);
+                inputData = Console.ReadLine()
+                .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+
+            foreach (var city in clubInfo)
+            {
+                Console.WriteLine(city.Key);
+                foreach (var pair in city.Value)
+                {
+                    Console.WriteLine("->{0}: {1}", pair.Key, string.Join(", ", pair.Value));
+                }
+            }
+        }
+    }
+}
