908b9eb068c4a2850aaa23e0d6fa5e2ee6736286
DeianH94 <deianh94@gmail.com>
2016-03-13 16:58:43 +0200

    Added problems 5 and 6.

---
+++ 02.MatrixSetsAndDictioneries/MatixSetsAndDictionariesHomework/Problem05.CollectTheCoins/CollectTheCoinsMain.cs
@@ -0,0 +1,58 @@
+﻿namespace Problem05.CollectTheCoins
+{
+    using System;
+
+    class CollectTheCoinsMain
+    {
+        static void Main()
+        {
+            char[][] jaggedArray = new char[4][];
+
+            for (int i = 0; i < jaggedArray.GetLength(0); i++)
+            {
+                jaggedArray[i] = Console.ReadLine().ToCharArray();
+            }
+
+            char[] movementCommands = Console.ReadLine().ToCharArray();
+
+            int currentRow = 0;
+            int currentCol = 0;
+            int coinsCollected = 0;
+            int wallsHit = 0;
+
+            foreach (char command in movementCommands)
+            {
+                if (command == '>' && jaggedArray[currentRow].Length - 1 > currentCol)
+                {
+                    currentCol++;
+                }
+                else if (command == '<' && currentCol > 0)
+                {
+                    currentCol--;
+                }
+                else if (command == '^' && currentRow > 0 
+                    && jaggedArray[currentRow - 1].Length > currentCol)
+                {
+                    currentRow--;
+                }
+                else if (command == 'V' && currentRow < 4 
+                    && jaggedArray[currentRow + 1].Length > currentCol)
+                {
+                    currentRow++;
+                }
+                else
+                {
+                    wallsHit++;
+                }
+
+                if (jaggedArray[currentRow][currentCol].Equals('$'))
+                {
+                    coinsCollected++;
+                }
+            }
+
+            Console.WriteLine("Walls hit: {0}", wallsHit);
+            Console.WriteLine("Coins collected: {0}", coinsCollected);
+        }
+    }
+}
---
+++ 02.MatrixSetsAndDictioneries/MatixSetsAndDictionariesHomework/Problem06.CountSymbols/CountSymbolsMain.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem06.CountSymbols
+{
+    using System;
+    using System.Collections.Generic;
+
+    class CountSymbolsMain
+    {
+        static void Main()
+        {
+            string inputString = Console.ReadLine();
+            var chars = new SortedDictionary<char, int>();
+
+            foreach (var ch in inputString)
+            {
+                if (chars.ContainsKey(ch))
+                {
+                    chars[ch]++;
+                }
+                else
+                {
+                    chars[ch] = 1;
+                }
+            }
+
+            foreach (var pair in chars)
+            {
+                Console.WriteLine("{0}: {1} time/s", pair.Key, pair.Value);
+            }
+        }
+    }
+}
