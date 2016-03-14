256547bc6f21180901cd4ed164c5bc23e5a09ac9
DeianH94 <deianh94@gmail.com>
2016-03-14 08:33:14 +0200

    Added all exercises about methods.

---
+++ 03.Methods/MethodsExercises/Problem01.LetterCreator/LetterCreatorMain.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem01.LetterCreator
+{
+    using System;
+
+    public class LetterCreatorMain
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Input the name of the receiver:");
+            string receiver = Console.ReadLine();
+            Console.WriteLine("Input the name of the sender:");
+            string sender = Console.ReadLine();
+
+            DateTime currentTime = DateTime.Today;
+            PrintLetter(sender, receiver, currentTime);
+        }
+
+        public static void PrintLetter(string sender, string receiver, DateTime currenTime)
+        {
+            Console.WriteLine("Dear {0},", receiver);
+            Console.WriteLine("I hope I find you in good health.");
+            Console.WriteLine("I need to inform you that the cheese has run away.");
+            Console.WriteLine("Sincerely, {0}", sender);
+            Console.WriteLine(currenTime.ToString("dd/MM/yyyy"));
+        }
+    }
+}
---
+++ 03.Methods/MethodsExercises/Problem02.UppercaseCharsAtEvenPositions/UppercaseCharsAtEvenPositionsMain.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem02.UppercaseCharsAtEvenPositions
+{
+    using System;
+
+    class UppercaseCharsAtEvenPositionsMain
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input a string:");
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine(UppercaseCharsAtEvenPositions(inputString));
+        }
+
+        public static string UppercaseCharsAtEvenPositions(string str)
+        {
+            char[] arr = str.ToCharArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    arr[i] = char.ToUpper(arr[i]);
+                }
+            }
+
+            return new string(arr);
+        }
+    }
+}
