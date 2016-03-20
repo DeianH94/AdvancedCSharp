2c59bbc3a2d5941cf500eb7e11903d87d1124de3
DeianH94 <deianh94@gmail.com>
2016-03-20 21:59:56 +0200

    Added the exercise about strings.

---
+++ 04.Strings/StringsExercise/CensorYourEmailAddress/CensorMain.cs
@@ -0,0 +1,25 @@
+﻿namespace CensorYourEmailAddress
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CensorMain
+    {
+        public static void Main(string[] args)
+        {
+            string text = "My name is Pesho Peshev. I am from Sofia, \n" 
+                          + "my email is: pesho.peshev@email.bg \n"
+                          + "(not pesho.peshev@email.com). Test: \n" 
+                          + "pesho.meshev@email.bg, pesho.peshev@email.bg";
+            string emailToCensor = "pesho.peshev@email.bg";
+
+            string replacementEmail = new string('*', emailToCensor.IndexOf("@")) 
+                + emailToCensor.Substring(emailToCensor.IndexOf("@"));
+            string replacedText = text.Replace(emailToCensor, replacementEmail);
+            Console.WriteLine(replacedText);
+        }
+    }
+}
