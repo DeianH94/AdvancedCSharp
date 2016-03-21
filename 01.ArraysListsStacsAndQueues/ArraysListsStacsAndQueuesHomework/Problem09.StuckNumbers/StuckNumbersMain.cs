86251b4371c18dfade195ee7d0a655c8954627fa
DeianH94 <deianh94@gmail.com>
2016-03-21 20:07:40 +0200

    Changes to StuckNumbers.cs to make it run faster.

--- 01.ArraysListsStacsAndQueues/ArraysListsStacsAndQueuesHomework/Problem09.StuckNumbers/StuckNumbersMain.cs
+++ 01.ArraysListsStacsAndQueues/ArraysListsStacsAndQueuesHomework/Problem09.StuckNumbers/StuckNumbersMain.cs
@@ -11,35 +11,51 @@
         public static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int[] nums = Console.ReadLine()
+            string[] nums = Console.ReadLine()
                     .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse)
                     .ToArray();
 
             int count = 0;
+            StringBuilder first = new StringBuilder();
+            StringBuilder second = new StringBuilder();
+            StringBuilder result = new StringBuilder();
             for (int firstNum = 0; firstNum < nums.Length; firstNum++)
             {
+                string a = nums[firstNum];
                 for (int secondNum = 0; secondNum < nums.Length; secondNum++)
                 {
+                    string b = nums[secondNum];
                     for (int thirdNum = 0; thirdNum < nums.Length; thirdNum++)
                     {
+                        string c = nums[thirdNum];
                         for (int fourthNum = 0; fourthNum < nums.Length; fourthNum++)
                         {
-                            int a = nums[firstNum];
-                            int b = nums[secondNum];
-                            int c = nums[thirdNum];
-                            int d = nums[fourthNum];
+                            string d = nums[fourthNum];
 
                             if (a != b && a != c & a != d &&
                             b != c && b != d && c != d)
                             {
-                                string first = a + b.ToString();
-                                string second = c + d.ToString();
-                                if (first == second)
+                                first.Append(a);
+                                first.Append(b);
+                                second.Append(c);
+                                second.Append(d);
+                                if (first.Equals(second))
                                 {
-                                    Console.Write("{0}|{1}=={2}|{3}\n", a, b, c, d);
+                                    result.Append(a);
+                                    result.Append("|");
+                                    result.Append(b);
+                                    result.Append("==");
+                                    result.Append(c);
+                                    result.Append("|");
+                                    result.Append(d);
+                                    result.Append("\n");
+                                    Console.Write(result);
                                     count++;
                                 }
+
+                                first.Clear();
+                                second.Clear();
+                                result.Clear();
                             }
                         }
                     }
